5a27090237058040e4e7536e93088a8f6f2418d5
Naim Mesias <naimmesias1968@hotmail.com>
2026-01-19 17:06:27 -0500

    feat: implement main application layout component with sidebar navigation and authentication features.

--- src/app/layout/main-layout/main-layout.component.ts
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AuthService } from '../../auth/auth.service';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -8,6 +8,10 @@ import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { CommonModule } from '@angular/common';
+import {
+  ContactInfoService,
+  ContactInfo,
+} from '../../services/contact-info.service';
 
 @Component({
   selector: 'app-main-layout',
@@ -27,9 +31,18 @@ import { CommonModule } from '@angular/common';
   templateUrl: './main-layout.component.html',
   styleUrl: './main-layout.component.css',
 })
-export class MainLayoutComponent {
+export class MainLayoutComponent implements OnInit {
   private authService = inject(AuthService);
+  private contactService = inject(ContactInfoService);
   isAuthenticated = this.authService.isAuthenticated;
+
+  contactInfo?: ContactInfo;
+
+  ngOnInit() {
+    this.contactService.getContactInfo().subscribe((info) => {
+      this.contactInfo = info;
+    });
+  }
 
   logout() {
     this.authService.logout();
